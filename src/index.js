20c254736f1a09ea2104fe7b94e38d027e39420d
Steven Grimshaw <stevengrimshaw@tamu.edu>
2021-11-21 16:27:18 -0600

    Fixed hotel list import statement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import HotelList from './HotelList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
