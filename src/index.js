f1e7a967f7c96c312ad4c6476964de4aa384db4c
Steven Grimshaw <stevengrimshaw@tamu.edu>
2021-11-21 17:25:51 -0600

    Removed hotel testing from main app page

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hotels from './Hotels';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Hotels />
   </React.StrictMode>,
   document.getElementById('root')
 );
