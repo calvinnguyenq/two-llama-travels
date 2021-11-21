fdf571c86d0b74f843214458dd552dda4de937fb
Steven Grimshaw <stevengrimshaw@tamu.edu>
2021-11-21 17:25:24 -0600

    Added scroll and pagination to hotels feature

--- src/Hotels.js
+++ src/Hotels.js
@@ -1,12 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import HotelList from ".HotelList";
+import DataTable from 'react-data-table-component';
+var axios = require("axios").default;
 
 function Hotels(props) {
+
+    const [apiResponse, setApiResponse] = useState([])
+
+    var options = {
+        method: 'GET',
+        url: 'https://hotels4.p.rapidapi.com/properties/list',
+        params: {
+          destinationId: '1506246',
+          pageNumber: '1',
+          pageSize: '25',
+          checkIn: '2020-01-08',
+          checkOut: '2020-01-15',
+          adults1: '1',
+          sortOrder: 'PRICE',
+          locale: 'en_US',
+          currency: 'USD'
+        },
+        headers: {
+          'x-rapidapi-host': 'hotels4.p.rapidapi.com',
+          'x-rapidapi-key': 'fc1f1ff2d3mshe2267e55a9681dfp1c29e0jsnfc471925dd78'
+        }
+      };
+    
+    useEffect(() => {
+      axios.request(options).then(function (response) {
+          setApiResponse(response.data.data.body.searchResults.results);
+          console.log(response.data.data.body.searchResults.results);
+      }).catch(function (error) {
+          console.error(error);
+      });
+    }, []);
+
+    const columns = [
+      {
+        name: 'Name',
+        selector: row => row.name,
+      },
+      {
+        name: 'Stars',
+        selector: row => row.starRating,
+        sortable: true,
+      },
+      {
+        name: 'Price',
+        selector: row => row.ratePlan.price.current + " per night",
+        sortable: true,
+      },
+    ];
+
     return (
-        <div>
-          <HotelList />
-        </div>
+        <DataTable
+          title="Hotels"
+          columns={columns}
+          data={apiResponse}
+          fixedHeader
+          fixedHeaderScrollHeight="300px"
+          progressPending={(apiResponse.length === 0)}
+          pagination
+          />
     );
 }
 
-export default HotelList;
+export default Hotels;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import HotelList from './HotelList';
+import Hotels from './Hotels';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <HotelList />
+    <Hotels />
   </React.StrictMode>,
   document.getElementById('root')
 );
